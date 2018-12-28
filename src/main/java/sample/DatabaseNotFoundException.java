b742b2b6164b6a282abd2d9402cc4f7c36febf65
Maciej <macrosa94@gmail.com>
2018-12-28 17:08:19 +0100

    * Integrated project with maven. * Added dependency instead of import for mysql connector. * Added deploying project as jar file.

---
+++ src/main/java/sample/DatabaseNotFoundException.java
@@ -0,0 +1,7 @@
+package sample;
+
+/**
+ * Created by Maciej on 2018-12-28 10:52
+ */
+public class DatabaseNotFoundException extends Exception {
+}
--- src/sample/DatabaseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-/**
- * Created by Maciej on 2018-12-28 10:52
- */
-public class DatabaseNotFoundException extends Exception {
-}
